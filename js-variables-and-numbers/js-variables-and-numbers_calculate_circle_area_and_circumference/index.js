cabf29d82085f7bf9ccbbcd4dbae9dab3003422e
Oliver Schupke <solanx@web.de>
2024-04-10 13:11:07 +0200

    Log message including computed values for circle area & circle circumference

--- js-variables-and-numbers/js-variables-and-numbers_calculate_circle_area_and_circumference/index.js
+++ js-variables-and-numbers/js-variables-and-numbers_calculate_circle_area_and_circumference/index.js
@@ -20,4 +20,9 @@ const circleRadius = 5;
 
 const circleArea = Math.PI * Math.pow(circleRadius, 2);
 
-const circumference =  2 * Math.PI * circleRadius;
+const circumference = 2 * Math.PI * circleRadius;
+
+console.log(`For a circle with a radius of ${circleRadius} units:
+
+- The area is approximately ${Math.round(circleArea * 100) / 100} square units.
+- The circumference is approximately ${Math.round(circumference * 100) / 100} units.`);
