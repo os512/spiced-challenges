522ff1c1ebb425bef145d320b29b574a11cbde61
Oliver Schupke <solanx@web.de>
2024-04-10 22:50:49 +0200

    Attach event listener to dark mode button

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,8 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const switchDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+
+switchDarkMode.addEventListener("click", function() {
+	bodyElement.classList.add('dark');
+})
