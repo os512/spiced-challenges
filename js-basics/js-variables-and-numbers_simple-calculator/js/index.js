1182c01ea2e332b33b3732b9c5efb31233b9cf83
Oliver Schupke <solanx@web.de>
2024-04-10 15:38:34 +0200

    Raise the two operands and log it

--- js-basics/js-variables-and-numbers_simple-calculator/js/index.js
+++ js-basics/js-variables-and-numbers_simple-calculator/js/index.js
@@ -39,6 +39,8 @@ divideButton.addEventListener("click", () => {
 
 exponentButton.addEventListener("click", () => {
   // 5. Raise "operand1" to the value of "operand2" and store the result in a variable. Log the variable's value to the console.
+  const exponential = operand1 ** operand2;
+  console.log("exponential: ", exponential);
 });
 
 moduloButton.addEventListener("click", () => {
