e204c324e2734e90ea5312eb11080c3dd8deda18
Oliver Schupke <solanx@web.de>
2024-04-10 15:36:42 +0200

    Divide the two operands and log it

--- js-basics/js-variables-and-numbers_simple-calculator/js/index.js
+++ js-basics/js-variables-and-numbers_simple-calculator/js/index.js
@@ -33,6 +33,8 @@ multiplyButton.addEventListener("click", () => {
 
 divideButton.addEventListener("click", () => {
   // 4. Divide the two operands and store the result in a variable. Log the variable's value to the console.
+  const dividend = operand1 / operand2;
+  console.log("dividend: ", dividend);
 });
 
 exponentButton.addEventListener("click", () => {
