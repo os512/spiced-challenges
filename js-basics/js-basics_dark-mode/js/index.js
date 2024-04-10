d413dac0a3a94792112357807295cecaf1899829
Oliver Schupke <solanx@web.de>
2024-04-10 22:54:39 +0200

    Attach event listener to remove dark mode from body

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -2,7 +2,12 @@ console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
 const switchDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+const switchLightMode = document.querySelector('[data-js="light-mode-button"]');
 
 switchDarkMode.addEventListener("click", function() {
 	bodyElement.classList.add('dark');
+})
+
+switchLightMode.addEventListener("click", function() {
+	bodyElement.classList.remove('dark');
 })
