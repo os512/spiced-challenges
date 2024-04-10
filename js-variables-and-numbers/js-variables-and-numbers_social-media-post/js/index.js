cebf017e01684f61ec80e2adcb070df7321cc94f
Oliver Schupke <solanx@web.de>
2024-04-10 11:50:51 +0200

    Log all values for variables defined in last commit

--- js-variables-and-numbers/js-variables-and-numbers_social-media-post/js/index.js
+++ js-variables-and-numbers/js-variables-and-numbers_social-media-post/js/index.js
@@ -13,7 +13,7 @@ console.clear();
 
 const title = "some title";
 const text = "some text";
-const numberOfLikes = 23;
+let numberOfLikes = 23;
 const user = {
   name: "John",
   postsCreated: 35,
@@ -27,5 +27,12 @@ const isReported = true;
 */
 
 // --v-- write your code here --v--
+console.log("title: ", title);
+console.log("text: ", text);
+console.log("numberOfLikes: ", numberOfLikes);
+numberOfLikes++;
+console.log("numberOfLikes: ", numberOfLikes);
+console.log("user: ", user);
+console.log("isReported: ", isReported);
 
 // --^-- write your code here --^--
