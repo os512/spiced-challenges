5e0a2da6e478c9ca7634dff1b55964153de00721
Oliver Schupke <solanx@web.de>
2024-04-16 15:42:38 +0200

    Add event listener and log all formData in js object format

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,9 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (e) => {
+  const formData = new FormData(e.target);
+  e.preventDefault();
+  console.log("Object.fromEntries(formData)): ", Object.fromEntries(formData));
+});
