71c7d50c48ceff539b8e2c9c449ac333df126efd
Oliver Schupke <solanx@web.de>
2024-04-16 15:44:09 +0200

    Add functionality: reset form after submit

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -5,5 +5,8 @@ const form = document.querySelector('[data-js="form"]');
 form.addEventListener("submit", (e) => {
   const formData = new FormData(e.target);
   e.preventDefault();
+  
   console.log("Object.fromEntries(formData)): ", Object.fromEntries(formData));
+
+  e.target.reset();
 });
