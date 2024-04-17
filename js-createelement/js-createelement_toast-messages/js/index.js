635c173a4d6b42bcb07f94a8d680295872d12ea8
Oliver Schupke <solanx@web.de>
2024-04-17 12:09:22 +0200

    Add functionality to remove list items (toasts) on click event

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -4,17 +4,25 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+function createElement() {
+  const listItem = document.createElement("li");
+  listItem.setAttribute("class", "toast-container__message");
+  listItem.textContent = "I'm a toast message.";
+
+  return listItem;
+}
+
 addButton.addEventListener("click", (e) => {
   // Exercise: Append a new entry to the toast messages container
   e.preventDefault();
 
-  const listItem = document.createElement("li");
-  listItem.setAttribute("class", "toast-container__message");
-
-  toastContainer.append(listItem);
-  listItem.textContent = "I'm a toast message.";
+  const listItemCreated = createElement();
+  toastContainer.append(listItemCreated);
 });
 
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", (e) => {
   // Exercise: Clear the stack of toast messages
+  e.preventDefault();
+
+  toastContainer.innerHTML = "";
 });
