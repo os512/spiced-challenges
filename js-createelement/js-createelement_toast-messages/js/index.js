d20aa9e6f4ddbc79d644ce1f01e933c3586b7574
Oliver Schupke <solanx@web.de>
2024-04-17 11:35:35 +0200

    Add functionality to create list items dynamically on click event

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -4,8 +4,15 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (e) => {
   // Exercise: Append a new entry to the toast messages container
+  e.preventDefault();
+
+  const listItem = document.createElement("li");
+  listItem.setAttribute("class", "toast-container__message");
+
+  toastContainer.append(listItem);
+  listItem.textContent = "I'm a toast message.";
 });
 
 clearButton.addEventListener("click", () => {
