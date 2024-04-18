ec5c4e6a0eb67ad3c4d78d8417590517d90b28ab
Oliver Schupke <solanx@web.de>
2024-04-18 20:33:29 +0200

    Add array products

---
+++ js-callback-functions/js-callback-functions_user-login-callback-5-calculating_the_total_price/index.js
@@ -0,0 +1,12 @@
+const products = [
+	{ name: "Saffron", price: 50 },
+	{ name: "Cinnamon", price: 6 },
+	{ name: "Cardamom", price: 12 },
+	{ name: "Turmeric", price: 9 },
+	{ name: "Paprika", price: 4 },
+	{ name: "Garam Masala", price: 10 },
+	{ name: "Vanilla Bean", price: 15 },
+	{ name: "Coriander", price: 6 },
+  ];
+
+  
