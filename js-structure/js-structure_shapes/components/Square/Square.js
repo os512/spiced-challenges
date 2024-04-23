dd5468277cb8b35e888b8429efcb3fc3e58baa80
Oliver Schupke <solanx@web.de>
2024-04-23 23:23:50 +0200

    Fix: random colors on shapes via click

--- js-structure/js-structure_shapes/components/Square/Square.js
+++ js-structure/js-structure_shapes/components/Square/Square.js
@@ -1,8 +1,10 @@
+import { getRandomColor } from "../../utils/randomColor.js";
+
 export default function Square() {
   const square = document.createElement("div");
   square.classList.add("square");
   square.addEventListener("click", () => {
-    square.style.backgroundColor = "#ccc";
+    square.style.backgroundColor = getRandomColor();
   });
 
   return square;
--- js-structure/js-structure_shapes/index.js
+++ js-structure/js-structure_shapes/index.js
@@ -1,4 +1,3 @@
-import { getRandomColor } from "./utils/randomColor.js";
 import Circle from "./components/Circle/Circle.js";
 import Square from "./components/square/square.js";
 import Pentagon from "./components/Pentagon/Pentagon.js";
