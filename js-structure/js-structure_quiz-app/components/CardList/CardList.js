2dc6c780d608cafa638f88de54b58335b0d5f44b
Oliver Schupke <solanx@web.de>
2024-04-23 16:18:01 +0200

    Refactor: Outsource function to its own component: CardList.js

---
+++ js-structure/js-structure_quiz-app/components/CardList/CardList.js
@@ -0,0 +1,11 @@
+export default function CardList() {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
