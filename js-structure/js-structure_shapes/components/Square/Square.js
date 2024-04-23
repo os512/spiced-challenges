52e6c05be04628071bf6a615c9e5e00a11e1ce55
Oliver Schupke <solanx@web.de>
2024-04-23 15:52:00 +0200

    Refactor: Outsource create square and pentagon functionality into their own modules

---
+++ js-structure/js-structure_shapes/components/Square/Square.js
@@ -0,0 +1,9 @@
+export default function Square() {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  square.addEventListener("click", () => {
+    square.style.backgroundColor = "#ccc";
+  });
+
+  return square;
+}
