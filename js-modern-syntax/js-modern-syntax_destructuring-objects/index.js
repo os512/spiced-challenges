73bed87572be2e0052069656ed144bb7e5ff739f
Oliver Schupke <solanx@web.de>
2024-04-26 16:25:23 +0200

    Solve task 5

--- js-modern-syntax/js-modern-syntax_destructuring-objects/index.js
+++ js-modern-syntax/js-modern-syntax_destructuring-objects/index.js
@@ -9,7 +9,7 @@ const company = {
   course: "Web Development",
   location: "Berlin",
 };
-export const {course} = company;
+export const { course } = company;
 // console.log(course);
 
 // example: export const {value1} = myObject;
@@ -23,7 +23,7 @@ export const {course} = company;
 
 const user = { name: "John", years: 30 };
 
-export const {name, years: age, isAdmin = false} = user;
+export const { name, years: age, isAdmin = false } = user;
 // console.log(isAdmin);
 
 // EXERCISE 3
@@ -36,9 +36,8 @@ const dog = {
   age: 5,
 };
 
-export const {name: dogName, breed: dogBreed, age: dogAge} = dog;
+export const { name: dogName, breed: dogBreed, age: dogAge } = dog;
 // console.log(dogAge);
-
 
 // EXERCISE 4
 // Extract the 'lastName' property from the person object as "personLastName".
@@ -50,7 +49,7 @@ const person = {
   firstName: "Alex",
 };
 
-export const {lastName: personLastName, ...moreInformation} = person;
+export const { lastName: personLastName, ...moreInformation } = person;
 // console.log(moreInformation);
 
 // EXERCISE 5
@@ -61,17 +60,21 @@ const logInfo = (city) => {
   const country = city.country;
   const numPeople = city.population;
 
-  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`
-}
+  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`;
+};
 
-const destructuringLogInfo = (city) => {
+export const destructuringLogInfo = (city) => {
   // (Hint: Lines 48-50 should be replaced with a single line and you shouldn't change anything else).
   // your code here
-
+  const { name, country, population: numPeople } = city;
 
   // your code here
-  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`
-}
+  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`;
+};
 
 // This is how you would call it:
- destructuringLogInfo({ name: "Marseille", country: "France", population: 861635 });
+destructuringLogInfo({
+  name: "Marseille",
+  country: "France",
+  population: 861635,
+});
