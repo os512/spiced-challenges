11a0b15e472a28ed93a3201bf9b9ea751c9e39b7
Oliver Schupke <solanx@web.de>
2024-04-29 11:44:09 +0200

    Feat: Add test cases for subtract() function

--- js-unit-testing/js-unit-testing_math-functions/index.test.js
+++ js-unit-testing/js-unit-testing_math-functions/index.test.js
@@ -1,7 +1,8 @@
 import { add } from "./index.js";
+import { subtract } from "./index.js";
 
 // Test add() function
-// 
+//
 describe("Add function tests", () => {
   test("returns 5 if called with add(2, 3)", () => {
     const result = add(2, 3);
@@ -15,9 +16,25 @@ describe("Add function tests", () => {
     expect(result).toBeLessThan(0);
   });
 
-  test('returns a value close to 0.3 if called with add(0.1, 0.2)', () => {
-	const result = add(0.1, 0.2);
+  test("returns a value close to 0.3 if called with add(0.1, 0.2)", () => {
+    const result = add(0.1, 0.2);
 
-	expect(result).toBeCloseTo(0.3);
+    expect(result).toBeCloseTo(0.3);
+  });
+});
+
+// Test subtract() function
+//
+describe("Add function tests", () => {
+  test("returns 10 if called with subtract(15, 5)", () => {
+    const result = subtract(15, 5);
+
+    expect(result).toBe(10);
+  });
+
+  test('returns a negative value if the second argument is greater than the first one', () => {
+	const result = subtract(3, 7);
+
+	expect(result).toBeLessThan(0);
   })
 });
