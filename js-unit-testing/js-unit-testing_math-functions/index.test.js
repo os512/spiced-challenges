7dbd3baec2baad6d8ef4fef953e8d88037e4a0ff
Oliver Schupke <solanx@web.de>
2024-04-29 11:55:48 +0200

    Feat: Add test cases for multiply() function

--- js-unit-testing/js-unit-testing_math-functions/index.test.js
+++ js-unit-testing/js-unit-testing_math-functions/index.test.js
@@ -1,9 +1,8 @@
-import { add } from "./index.js";
-import { subtract } from "./index.js";
+import { add, subtract, multiply } from "./index.js";
 
 // Test add() function
 //
-describe("Add function tests", () => {
+describe("Add function tests for add()", () => {
   test("returns 5 if called with add(2, 3)", () => {
     const result = add(2, 3);
 
@@ -25,16 +24,44 @@ describe("Add function tests", () => {
 
 // Test subtract() function
 //
-describe("Add function tests", () => {
+describe("Add function tests for subtract()", () => {
   test("returns 10 if called with subtract(15, 5)", () => {
     const result = subtract(15, 5);
 
     expect(result).toBe(10);
   });
 
-  test('returns a negative value if the second argument is greater than the first one', () => {
-	const result = subtract(3, 7);
+  test("returns a negative value if the second argument is greater than the first one", () => {
+    const result = subtract(3, 7);
 
-	expect(result).toBeLessThan(0);
-  })
+    expect(result).toBeLessThan(0);
+  });
+});
+
+// Test multiply() function
+//
+describe("Add function tests for multiply()", () => {
+  test("returns 8 if called with multiply(2, 4)", () => {
+    const result = multiply(2, 4);
+
+    expect(result).toBe(8);
+  });
+
+  test("returns a negative value if only the first argument is negative", () => {
+    const result = multiply(-2, 3);
+
+    expect(result).toBeLessThan(0);
+  });
+
+  test("returns a negative value if only the second argument is negative", () => {
+    const result = multiply(2, -3);
+
+    expect(result).toBeLessThan(0);
+  });
+
+  test("returns a positive value if called with two negative arguments", () => {
+    const result = multiply(-2, -3);
+
+    expect(result).toBeGreaterThan(0);
+  });
 });
