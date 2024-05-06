01e9a45c88b7b9f7f4dd82967ad6aeb6751c4464
Oliver Schupke <solanx@web.de>
2024-05-06 16:31:55 +0200

    Refactor: Implement header component

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -1,35 +1,36 @@
 import "./App.css";
+import Header from "../src/components/Header.jsx";
 
 import avatar from "./assets/avatar.jpg";
 import logo from "./assets/logo.jpg";
 
 export default function App() {
-  return (
-    <>
-      <header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
-        <nav>
-          <a className="navigation__link" href="#home">
-            Home
-          </a>
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#impressum">
-            Impressum
-          </a>
-        </nav>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
-      </header>
-      <main>content goes here…</main>
-    </>
-  );
+	return (
+		<>
+			<Header>
+				<a href="#">
+					<img className="round-image" src={logo} alt="logo" />
+				</a>
+				<nav>
+					<a className="navigation__link" href="#home">
+						Home
+					</a>
+					<a className="navigation__link" href="#about">
+						About
+					</a>
+					<a className="navigation__link" href="#impressum">
+						Impressum
+					</a>
+				</nav>
+				<button
+					type="button"
+					onClick={() => console.log("I could toggle a profile!")}
+					aria-label="toggle profile"
+				>
+					<img className="round-image" src={avatar} alt="avatar" />
+				</button>
+			</Header>
+			<main>content goes here…</main>
+		</>
+	);
 }
