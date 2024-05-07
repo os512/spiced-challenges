c14cca1f6a5c2d7291373cf3f67f7ce3fe250432
Oliver Schupke <solanx@web.de>
2024-05-07 11:50:28 +0200

    Chore: Add html markup for the whole page as jsx

--- react-project-setup/journal-app/src/App.jsx
+++ react-project-setup/journal-app/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import star_filled from "./assets/star-filled.svg";
+import star_transparent from "./assets/star.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			<header>
+				<h1>JOURNAL</h1>
+			</header>
+			<main>
+				<section>
+					<h2>NEW ENTRY</h2>
+					<form>
+						<label htmlFor="input__motto">Motto</label>
+						<input id="input__motto" type="text"></input>
+						<label htmlFor="input__notes">Notes</label>
+						<input id="input__notes" type="text"></input>
+						<button type="submit">Create</button>
+					</form>
+				</section>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+				<section>
+					<label htmlFor="count__entries">All Entries</label>
+					<p id="count__entries">3</p>
+					<label htmlFor="count__favorites">Favorites</label>
+					<p id="count__favorites">1</p>
+				</section>
+				<section className="section__journal">
+					<h3>FEB 27, 2028</h3>
+					<h4>&quot;That&apos;s life in the city&quot;</h4>
+					<img src={star_filled} alt="Star filled"></img>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, cumque assumenda
+						magni aliquid inventore amet cupiditate consequatur possimus officiis reiciendis ut
+						voluptatum labore laborum ipsum voluptas laudantium illum.
+					</p>
+				</section>
+
+				<section className="section__journal">
+					<h3>FEB 26, 2028</h3>
+					<h4>&quot;That&apos;s life in the city&quot;</h4>
+					<img src={star_transparent} alt="Star filled"></img>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, cumque assumenda
+						magni aliquid inventore amet cupiditate consequatur possimus officiis reiciendis ut
+						voluptatum labore laborum ipsum voluptas laudantium illum.
+					</p>
+				</section>
+
+        <footer>
+          journal App – 2028
+        </footer>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
