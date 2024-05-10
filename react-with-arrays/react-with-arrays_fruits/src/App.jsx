d65820a0986733a8fc6da65b13a6c3311e2dadce
Oliver Schupke <solanx@web.de>
2024-05-10 11:14:30 +0200

    Feat: Solve Challenge

--- react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,13 +1,43 @@
 import Card from "./components/Card";
 import "./App.css";
 
-
 export default function App() {
-  const fruits = [];
+	const fruits = [
+		{
+			id: 1337,
+			name: "Banana",
+			color: "yellow",
+		},
+		{
+			id: 1338,
+			name: "Apple",
+			color: "red",
+		},
+		{
+			id: 1339,
+			name: "Grapefruit",
+			color: "orange",
+		},
+		{
+			id: 1340,
+			name: "Lime",
+			color: "green",
+		},
+		{
+			id: 1341,
+			name: "Kiwi",
+			color: "green",
+		},
+	];
 
-  return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
-  );
+	return (
+		<div className="app">
+			{fruits.map((fruit) => (
+				<li key={fruit.id}>
+					<Card color={fruit.color} name={fruit.name} />
+					{console.log(Card)}
+				</li>
+			))}
+		</div>
+	);
 }
