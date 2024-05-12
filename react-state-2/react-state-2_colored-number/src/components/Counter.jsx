a769061003de5c24633bf7dc758663e4edd16f28
Oliver Schupke <solanx@web.de>
2024-05-12 15:56:54 +0200

    wip

--- react-state-2/react-state-2_colored-number/src/components/Counter.jsx
+++ react-state-2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,12 +1,15 @@
 import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
 import "./Counter.css";
 
-
 export default function Counter() {
+  let [counter, SetCounter] = useState(0);
+
   return (
     <div className="counter">
       <h1>Colored Number</h1>
       <ColoredNumber value={0} />
+      {/* <ColoredNumber value={SetCounter(counter++)} /> */}
       <div className="counter__buttons-container">
         <button
           type="button"
