73a017acd09c533ab4d48ae29598604624477d46
Oliver Schupke <solanx@web.de>
2024-05-14 01:27:58 +0200

    Refactor: Add props parameters

--- react-state-3/react-state-3_journal-app-favorite-button-shared-state/src/components/Entry/index.jsx
+++ react-state-3/react-state-3_journal-app-favorite-button-shared-state/src/components/Entry/index.jsx
@@ -1,19 +1,19 @@
 import FavoriteButton from "../FavoriteButton";
 import "./Entry.css";
 
-export default function Entry({ motto, notes, date }) {
-  return (
-    <article className="entry">
-      <time className="entry__date">{date}</time>
-      <div className="entry__content">
-        <div className="entry__motto-container">
-          <h2 className="entry__motto">
-            <q>{motto}</q>
-          </h2>
-          <FavoriteButton />
-        </div>
-        <p className="entry__notes">{notes}</p>
-      </div>
-    </article>
-  );
+export default function Entry({ motto, notes, date, onToggleFavorite, isFavorite, id }) {
+	return (
+		<article className="entry">
+			<time className="entry__date">{date}</time>
+			<div className="entry__content">
+				<div className="entry__motto-container">
+					<h2 className="entry__motto">
+						<q>{motto}</q>
+					</h2>
+					<FavoriteButton onToggleFavorite={onToggleFavorite} isFavorite={isFavorite} id={id}/>
+				</div>
+				<p className="entry__notes">{notes}</p>
+			</div>
+		</article>
+	);
 }
