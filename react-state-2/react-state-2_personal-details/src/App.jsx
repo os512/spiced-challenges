c687f3007080c80902fd4b39c01ecdad5403a630
Oliver Schupke <solanx@web.de>
2024-05-13 20:20:16 +0200

    Feat: Solve challenge

--- react-state-2/react-state-2_personal-details/src/App.jsx
+++ react-state-2/react-state-2_personal-details/src/App.jsx
@@ -1,17 +1,28 @@
-import "./App.css";
 import Form from "./components/Form.jsx";
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
+  const [name, setName] = useState(""),
+    [email, setEmail] = useState("");
+
+  function handleCreateUser(name, email) {
+    setName(name);
+    setEmail(email);
+  }
+
   return (
     <div className="container">
       <h1>Personal Details Form</h1>
-      <Form />
+
+      <Form onCreateUser={handleCreateUser} />
+
       <h2>Your submitted details:</h2>
       <p>
-        Name: <span className="output">John doe</span>
+        Name: <span className="output">{name}</span>
       </p>
       <p>
-        Email: <span className="output">John@Doe.com</span>
+        Email: <span className="output">{email}</span>
       </p>
     </div>
   );
