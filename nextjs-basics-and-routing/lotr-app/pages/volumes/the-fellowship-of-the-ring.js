9a72b8cd87e86f34a8a91a254bb63cb4e12c3b88
Oliver Schupke <solanx@web.de>
2024-05-17 13:56:29 +0200

    Refactor: Implement volume details pages

---
+++ nextjs-basics-and-routing/lotr-app/pages/volumes/the-fellowship-of-the-ring.js
@@ -0,0 +1,35 @@
+import { volumes } from "../lib/data";
+import Link from "next/link";
+import Image from "next/image";
+
+const volume = volumes.find(
+  (volume) => volume.slug === "the-fellowship-of-the-ring"
+);
+
+export default function TheFellowshipOfTheRing() {
+  return (
+    <>
+      <Link href="./">Back to Volumes</Link>
+      <h1>{volume.title}</h1>
+      <p>{volume.description}</p>
+      <ul>
+        {volume.books.map((book, idx) => {
+          return (
+            <div key={idx}>
+              <li>
+                <p>{book.ordinal}</p>
+                <p>{book.title}</p>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+      <Image
+        src={volume.cover}
+        alt={volume.title}
+        width="140"
+        height="230"
+      ></Image>
+    </>
+  );
+}
---
+++ nextjs-basics-and-routing/lotr-app/pages/volumes/the-two-towers.js
@@ -0,0 +1,31 @@
+import { volumes } from "../lib/data";
+import Link from "next/link";
+import Image from "next/image";
+
+const volume = volumes.find((volume) => volume.slug === "the-two-towers");
+
+export default function TheTwoTowers() {
+  return (
+    <>
+      <Link href="./">Back to Volumes</Link>
+      <h1>{volume.title}</h1>
+      <p>{volume.description}</p>
+      <ul>
+        {volume.books.map((book, idx) => {
+          return (
+            <li key={idx}>
+              <p>{book.ordinal}</p>
+              <p>{book.title}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <Image
+        src={volume.cover}
+        alt={volume.title}
+        width="140"
+        height="230"
+      ></Image>
+    </>
+  );
+}
