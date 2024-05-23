94cc3e6fb7671cc1bec33911f96e83f45daa35e9
Oliver Schupke <solanx@web.de>
2024-05-23 20:19:24 +0200

    Feat: Add 5 sec refresh rate for updating the ISS position

--- react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -16,26 +16,26 @@ const fetcher = async (url) => {
     error.status = res.status;
     throw error;
   }
-
   return res.json();
 };
 
 export default function ISSTracker() {
   const { data, error, isLoading } = useSWR(URL, fetcher);
 
+  useSWR(URL, fetcher, { refreshInterval: 5000 });
+
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
 
+  // ⚠️ The ⬆︎ `isLoading` condition ⬆︎ is crucial for a successful destructuring
+  // of the `data` object. Without it, destructuring of the `data` object will
+  // fail since it's undefined on the initial pageload!
   const { longitude, latitude } = data;
 
   return (
     <main>
       <Map longitude={longitude} latitude={latitude} />
-      <Controls
-        longitude={longitude}
-        latitude={latitude}
-        // onRefresh={getISSCoords}
-      />
+      <Controls longitude={longitude} latitude={latitude} />
     </main>
   );
 }
