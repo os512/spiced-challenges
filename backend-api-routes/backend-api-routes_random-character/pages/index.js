88ff95dc0da1f76e2957dc22c34c1189dc6dc490
Oliver Schupke <solanx@web.de>
2024-05-29 16:21:48 +0200

    Feat: Implement frontend related logic

--- backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,34 @@
+import useSWR from "swr";
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const URL = "/api/random-character";
+
+  const fetcher = async (url) => {
+    const data = await fetch(url);
+
+    if (!data.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+      error.info = await data.json();
+      error.status = data.status;
+      throw error;
+    }
+
+    return data.json();
+  };
+
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  const { firstName, lastName, twitterName, geohash } = data;
+
+  return (
+    <>
+      <p>firstname: {firstName}</p>
+      <p>lastName: {lastName}</p>
+      <p>twitterName: {twitterName}</p>
+      <p>geohash: {geohash}</p>
+    </>
+  );
 }
