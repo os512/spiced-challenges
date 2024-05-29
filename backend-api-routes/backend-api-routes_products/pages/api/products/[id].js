48f2ac7e14a66a44b18e786a43822cdd47bfa238
Oliver Schupke <solanx@web.de>
2024-05-29 18:39:16 +0200

    Style: Remove unused console.log statements

--- backend-api-routes/backend-api-routes_products/pages/api/products/[id].js
+++ backend-api-routes/backend-api-routes_products/pages/api/products/[id].js
@@ -1,13 +1,7 @@
 import { getProductById } from "@/services/productServices.js";
 
 export default function handler(req, res) {
-//   console.log("request in ID getProductById: ", req.query);
-  console.log("dshjfdskfhdjskfhsdjkfhsdfjkdslhfjksdfhsjklxs");
-
   const { id } = req.query;
-
-  console.log(id);
-
   const product = getProductById(id);
 
   if (!product) {
--- backend-api-routes/backend-api-routes_products/pages/api/products/index.js
+++ backend-api-routes/backend-api-routes_products/pages/api/products/index.js
@@ -1,6 +1,5 @@
 import { getAllProducts } from "@/services/productServices.js";
 
 export default function handler(request, response) {
-	console.log(getAllProducts());
 	response.status(200).json(getAllProducts());
   }
